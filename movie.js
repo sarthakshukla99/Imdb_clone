5556b75bcd806d6caeb31d68744c94f04fe21bb0
Sarthak Shukla <sarthakshukla72@gmail.com>
2022-09-11 22:11:45 +0530

    minor imporvements in code

--- movie.js
+++ movie.js
@@ -1,25 +1,24 @@
 let favm = document.getElementById("movie-detail");
 // here we will write function when user click on detail button and we will fetch current movie saved in local storage and display it.
 function renderMovie() {
-  let movieList = document.querySelector("#movie-detail");
-  if (movieList) movieList.innerHTML = "";
-  let favmovie = localStorage.getItem("current_movie");
-  console.log(favmovie);
-  let savedMovie = JSON.parse(favmovie);
-  if (savedMovie == null) {
-    favmovie = [];
-  } else {
-    
-      const avatar = savedMovie.Title;
-      const imgsrc = savedMovie.Poster;
-      const plot = savedMovie.Plot;
-      const released = savedMovie.Released;
-      const imdb = savedMovie.imdbRating;
-      const runtime= savedMovie.Runtime;
-      const genre= savedMovie.Genre;
-      const writer= savedMovie.Writer;
+    let movieList = document.querySelector("#movie-detail");
+    if (movieList) movieList.innerHTML = "";
+    let favmovie = localStorage.getItem("current_movie");
+    console.log(favmovie);
+    let savedMovie = JSON.parse(favmovie);
+    if (savedMovie == null) {
+        favmovie = [];
+    } else {
+        const avatar = savedMovie.Title;
+        const imgsrc = savedMovie.Poster;
+        const plot = savedMovie.Plot;
+        const released = savedMovie.Released;
+        const imdb = savedMovie.imdbRating;
+        const runtime = savedMovie.Runtime;
+        const genre = savedMovie.Genre;
+        const writer = savedMovie.Writer;
 
-      favm.innerHTML += `
+        favm.innerHTML += `
       <div class="card my-5 " style="width: 70%"; margin:'auto';  >
         <img src="${imgsrc}" class="" alt="...">
         <div class="card-body">
@@ -34,10 +33,6 @@ function renderMovie() {
           <li class="list-group-item"><b>GENRE</b>: ${genre}</li>
         </ul>
       </div>`;
-
-      
-      
-    
-  }
+    }
 }
 renderMovie();
--- script.js
+++ script.js
@@ -1,60 +1,61 @@
 console.log("hello");
 
-
 let access_token = "fd573a90";
 const search = document.getElementById("search");
 const suggestion = document.getElementById("match-list");
 let currentMovie = {};
-// this function will add the clicked movie into the favourite list in local staorage 
+
+// this function will add the clicked movie into the favourite list in local staorage
 function favMovie(e) {
-  const first = e.target.name.split(" ");
-  const movieName = first[0] + first[1];
-  console.log("details", movieName);
-  if (e.target.innerHTML == "Favourite") {
-    e.target.innerHTML = "Unfavourite";
-    let favmovie = JSON.parse(localStorage.getItem("favMovie")) || [];
-    let results = JSON.parse(localStorage.getItem("results")) || [];
-    favmovie.push(results[Number(e.target.id)]);
-    localStorage.setItem("favMovie", JSON.stringify(favmovie));
-    e.target.value = " ";
-  }
+    const first = e.target.name.split(" ");
+    const movieName = first[0] + first[1];
+    console.log("details", movieName);
+    if (e.target.innerHTML == "Favourite") {
+        e.target.innerHTML = "Unfavourite";
+        let favmovie = JSON.parse(localStorage.getItem("favMovie")) || [];
+        let results = JSON.parse(localStorage.getItem("results")) || [];
+        favmovie.push(results[Number(e.target.id)]);
+        localStorage.setItem("favMovie", JSON.stringify(favmovie));
+        e.target.value = " ";
+    }
 }
 
+// this function is used to redirect to movie details page
 function movieDetails(event) {
-  let results = JSON.parse(localStorage.getItem("results")) || [];
-  let current_movie = results[Number(event.target.id)];
-  localStorage.setItem("current_movie", JSON.stringify(current_movie));
-  window.location.assign("movie.html");
+    let results = JSON.parse(localStorage.getItem("results")) || [];
+    let current_movie = results[Number(event.target.id)];
+    localStorage.setItem("current_movie", JSON.stringify(current_movie));
+    window.location.assign("movie.html");
 }
 // this will fetch the api of words typed by the user whenever it enters the words in input tag
 search.addEventListener("input", (e) => {
-  const fetchApi = async function () {
-    const response = await fetch(
-        `https://www.omdbapi.com/?t=${e.target.value}&apikey=${access_token}`
-    );
-    const data = await response.json();
-    console.log(data);
-    //  Object.entries(data).forEach(v => {
-    let results = JSON.parse(localStorage.getItem("results")) || [];
-    results.push(data);
-    localStorage.setItem("results", JSON.stringify(results));
-    const avatar = data.Title;
-    const imgsrc = data.Poster;
-    currentMovie = data;
-    suggestion.innerHTML = `
+    const fetchApi = async function () {
+        const response = await fetch(
+            `https://www.omdbapi.com/?t=${e.target.value}&apikey=${access_token}`
+        );
+        const data = await response.json();
+        console.log(data);
+        //  Object.entries(data).forEach(v => {
+        let results = JSON.parse(localStorage.getItem("results")) || [];
+        results.push(data);
+        localStorage.setItem("results", JSON.stringify(results));
+        const avatar = data.Title;
+        const imgsrc = data.Poster;
+        currentMovie = data;
+        suggestion.innerHTML = `
           <div class="card-body">
         <h5 class="card-title">${avatar}</h5>
         <img src="${imgsrc}">
-        <button class="btn btn-primary" id="${results.length - 1}" name=${JSON.stringify(
-      data
-    )} onclick="favMovie(event)">Favourite</button>
         <button class="btn btn-primary" id="${
-          results.length - 1
+            results.length - 1
+        }" name=${JSON.stringify(
+            data
+        )} onclick="favMovie(event)">Favourite</button>
+        <button class="btn btn-primary" id="${
+            results.length - 1
         }" onclick=movieDetails(event)>Details</button>
       </div>`;
+    };
 
-   
-  };
-
-  fetchApi();
+    fetchApi();
 });
